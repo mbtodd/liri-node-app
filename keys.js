b50126c5be2b42c2fef97daa1f4b5c3f0aad3007
Michael Todd <michaelbtodd@yahoo.com>
2018-04-20 22:15:27 -0400

    keys.js, liri.js, json updated

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-console.log('this is loaded');
+console.log('this is loaded and working');
 
 exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -11,3 +11,9 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.omdb = {
+  omdb_key: process.env.OMDB_KEY,
+  omdb_api: process.env.OMDb_API,
+  poster_api: process.env.POSTER_API,
+}
--- liri.js
+++ liri.js
@@ -1,22 +1,23 @@
-import { twitter } from "./keys";
+// import { twitter } from "./keys";
+
+
 
 require("dotenv").config();
 
-var textFile = process.argv[2];
-var keys = require('keys.js');
+var textFile = process.argv;
+var spotify = require('node-spotify-api');
+var keys = require('./keys.js');
+var twitter = require('twitter');
+var request = require('request');
+var fs = require('fs');
 
-keys.readFile(textFile, 'Spotify this song', function(err){
-    if (err){
-        console.log (err)
-    }
-    else {
-        console.log('This is beginnig to work')
-    }
-})
+console.log('Liri page is working now')
+
+console.log("my-tweets, spotify-this-song, movie-this, do-what-it-says");
 
 
-var spotify = new spotify(keys.spotify);
-var client = new twitter(keys.twitter);
+// var spotify = new spotify(keys.spotify);
+// var client = new twitter(keys.twitter);
 
 
 
