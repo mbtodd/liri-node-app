347a0f3118bece48b09fedde450b096d687c6236
Michael Todd <michaelbtodd@yahoo.com>
2018-04-21 22:05:11 -0400

    gitting liri.js to work/ not completed

--- liri.js
+++ liri.js
@@ -1,3 +1,5 @@
+import { createServer } from "http";
+
 // import { twitter } from "./keys";
 
 
@@ -11,8 +13,8 @@ var twitter = require('twitter');
 var request = require('request');
 var fs = require('fs');
 
-var spotify = new spotify(keys.spotify);
-var client = new twitter(keys.twitter);
+// var spotify = new spotify(keys.spotify);
+// var client = new twitter(keys.twitter);
 
 
 console.log('Liri page is working now')
@@ -22,15 +24,59 @@ var userCommand = process.argv[2];
 var secondCommand = process.argv[3];
 
 for (i = 4; i < process.argv.length; i++) {
-     secondCommand += '+' + process.argv[i];
-    
+    secondCommand += '+' + process.argv[i];
+
 }
 
-function theGreatSwitch() { switch(userCommand){ case 'my-tweets': fetchTweets(); break}};
+function switchStatement() {
+    switch (userCommand) {
+        case 'my-tweets':
+            fetchTweets();
+            break;
 
+        case 'spotify-this-song':
+            spotifyMe();
+            break;
 
+        case 'movie-this':
+            movieMe();
+            break;
 
+        case 'do-what-it-says':
+            followTheTextbook();
+            break;
 
+    }
+};
+
+function fetchTweets() {
+    console.log("The is the Tweet!");
+
+    var client = new twitter({
+        consumer_key: key.twitterKeys.consumer_key,
+        consumer_secret: keys.twitterKeys.consumer_secret,
+        access_token_key:keys.twitterKeys.access_token_key,
+        access_token_secret: keys.twitterkeys.access_token_secret,
+        
+
+    });
+
+    
+    var parameters = {
+        screen_name:'multishifities',
+        count: 20
+    };
+
+    client.get('statuses/user_timeline', parameters, function (err, tweets, response) {
+        if (!err) {
+            for ( i = 0; i < tweets.length; i++) {
+                var returnedData = ('Number: ' +(i + 1)+ '\n' + tweets[i].created_at + '\n' + tweets[i].text + '\n');
+                console.log(returnedData);
+                console.log('---------------------');
+            }
+        };
+    });
+};
 
 
 
